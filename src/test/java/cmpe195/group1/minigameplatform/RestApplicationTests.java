10c107cbf3dfb27a1483837ce2305dccdd9ea2dd
airsquared <36649395+airsquared@users.noreply.github.com>
2026-04-17 20:30:31 -0700

    backend: Reduce log spam

--- src/test/java/cmpe195/group1/minigameplatform/RestApplicationTests.java
+++ src/test/java/cmpe195/group1/minigameplatform/RestApplicationTests.java
@@ -1,9 +1,7 @@
 package cmpe195.group1.minigameplatform;
 
-import cmpe195.group1.minigameplatform.rest.PublicController;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import static org.assertj.core.api.Assertions.*;
 
 
 @SpringBootTest
@@ -11,11 +9,6 @@ class RestApplicationTests {
 
     @Test
     void contextLoads() {
-    }
-
-    @Test
-    void indexTest() {
-        assertThat(new PublicController().index()).isEqualTo("Welcome to the Minigame Platform API!");
     }
 
 }
